8ee5d387a4dfaf563c8732d55c442d86b0ef9cd3
Nicolas "0swin" Roger <nicolas@oswin.io>
2019-05-14 17:40:40 +0200

    refactor

--- Dev/JSONSortingExample/sketch.js
+++ Dev/JSONSortingExample/sketch.js
@@ -16,7 +16,6 @@ function setup() {
         title = title.replace(/[0-9]/g, ""); // VIRER LES CHIFFRES
         title = title.normalize('NFD').replace(/[\u0300-\u036f]/g, ""); // VIRER LES ACCENTS
         let firstWord = title.split(regex); // VIRER LA PONCTUATION
-        let finalWord = "";
         // NE GARDER QUE LES MOTS QUI FONT QUE PLUS DE X CARACTERES
         for (let j = 0; j < firstWord.length; j++) {
             let word = firstWord[j];
@@ -28,8 +27,6 @@ function setup() {
         console.log(finalWord.split(""))
         // countPer(finalWord, dictionary)
     }
-
-    console.log(dictionary)
 }
 
 function draw() {}
