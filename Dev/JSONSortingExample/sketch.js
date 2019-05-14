1b05dca5ae3d27b83d1c360da8e07b0d526d1b4b
Nicolas "0swin" Roger <nicolas@oswin.io>
2019-05-14 15:02:24 +0200

    Tri premier mot de chaque titre

--- Dev/JSONSortingExample/sketch.js
+++ Dev/JSONSortingExample/sketch.js
@@ -1,31 +1,34 @@
-var url = "../../Data/gallica_audios.json"
+var url = "../../Data/gallica_videos.json"
 var datas
 var records
 
 function preload() {
     datas = loadJSON(url)
-    console.log(datas)
 }
 
 function setup() {
     records = datas.records
     let dictionary = {}
+    let regex = /\W+/;
     for (let i = 0; i < records.length; i++) {
-        let value = records[i].datestamp
-        // let subvalue = records[i].date
-        // subvalue = parseDate(value)
-        value = convertToDate(value)
-        countPer(value, dictionary)
+        let title = records[i].title
+        title = title.toLowerCase();
+        title = title.replace(/[0-9]/g, ""); // VIRER LES CHIFFRES
+        title = title.normalize('NFD').replace(/[\u0300-\u036f]/g, ""); // VIRER LES ACCENTS
+        let firstWord = title.split(regex); // VIRER LA PONCTUATION
+        let finalWord = "";
+        // NE GARDER QUE LES MOTS QUI FONT QUE PLUS DE X CARACTERES
+        for(let j= 0; j<firstWord.length; j++){
+            let word = firstWord[j];
+            if(word.length > 2){
+                finalWord = word;
+                break;
+            }
+        }
+        countPer(finalWord, dictionary)
     }
-    
-    let size = sizeOf(dictionary)
-    console.log(size)    
-    console.log(dictionary)
 
-    let array = convertDictionaryIntoJSONArray(dictionary)
-    let json = {}
-    json.records = array
-    saveJSON(json, "export.json")
+    console.log(dictionary)
 }
 
 function draw() {}
