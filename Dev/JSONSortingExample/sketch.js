82760ab98b132694958b2489767ecd4b8b65c41e
melilun <luntadilam@hotmail.fr>
2019-05-15 15:58:55 +0200

    Revert "Trace all first word of dictionnary"
    This reverts commit 233d2740d4cd0702901067840007b612d6625f87.

--- Dev/JSONSortingExample/sketch.js
+++ Dev/JSONSortingExample/sketch.js
@@ -11,34 +11,33 @@ function setup() {
     background(0)
 
     records = datas.records
-    let dictionary = []
-    let regex = /\W+/
+    let dictionary = {}
+    let regex = /\W+/;
     for (let i = 0; i < records.length; i++) {
         let title = records[i].title
-        title = title.toLowerCase()
+        title = title.toLowerCase();
         title = title.replace(/[0-9]/g, "") // VIRER LES CHIFFRES
-        title = title.normalize('NFD').replace(/[\u0300-\u036f]/g, "") // VIRER LES ACCENTS
+        title = title.normalize('NFD').replace(/[\u0300-\u036f]/g, ""); // VIRER LES ACCENTS
         let firstWord = title.split(regex) // VIRER LA PONCTUATION
         // NE GARDER QUE LES MOTS QUI FONT QUE PLUS DE X CARACTERES
         for (let j = 0; j < firstWord.length; j++) {
-            let word = firstWord[j]
+            let word = firstWord[j];
             if (word.length > 2) {
-                finalWord = word
+                finalWord = word;
                 break
             }
         }
-        dictionary.push(finalWord)
+        // console.log(finalWord.split(""))
     }
-    console.log(dictionary)
 
     let originx = width / 2
     let originy = height / 2
-    let radius = 150
+    let radius = 200
+    let nbpoint = 26
     let alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
-    let nbpoint = alphabet.length
 
     // GENERER LE CERCLE
-    noFill()
+    fill(255)
     beginShape()
     for (let j = 0; j < nbpoint; j++) {
         let angle = map(j, 0, nbpoint, 0 - HALF_PI, TWO_PI - HALF_PI)
@@ -52,34 +51,38 @@ function setup() {
     fill(255)
     for (let j = 0; j < nbpoint; j++) {
         let angle = map(j, 0, nbpoint, 0 - HALF_PI, TWO_PI - HALF_PI)
-        let px2 = cos(angle) * (radius + radius / 100 * 10) + originx
-        let py2 = sin(angle) * (radius + radius / 100 * 10) + originy
+        let px2 = cos(angle) * (radius + 10) + originx
+        let py2 = sin(angle) * (radius + 10) + originy
         textAlign(CENTER, CENTER);
         text(alphabet[j].toUpperCase(), px2, py2)
     }
 
-    for (let i = 0; i < dictionary.length; i++) {
-        let word = dictionary[i]
-        word = word.split("")
-        for (let i = 0; i < word.length; i++) {
-            let letterStart = alphabet.indexOf(word[i])
-            let angleStart = map(letterStart, 0, 26, 0 - HALF_PI, TWO_PI - HALF_PI)
-            let pxStart = cos(angleStart) * (radius) + originx
-            let pyStart = sin(angleStart) * (radius) + originy
+    // let angle = map(25, 0, 26, 0 - HALF_PI, TWO_PI - HALF_PI)
+    // let px2 = cos(angle) * (radius) + originx
+    // let py2 = sin(angle) * (radius) + originy
+    // rectMode(CENTER)
+    // rect(px2, py2, 10, 10)
 
-            let letterEnd = alphabet.indexOf(word[i + 1])
-            let angleEnd = map(letterEnd, 0, 26, 0 - HALF_PI, TWO_PI - HALF_PI)
-            let pxEnd = cos(angleEnd) * (radius) + originx
-            let pyEnd = sin(angleEnd) * (radius) + originy
+    let word = "gallica"
+    word = word.split("")
+    for (let i = 0; i < word.length; i++) {
+        let letterStart = alphabet.indexOf(word[i])
+        let angleStart = map(letterStart, 0, 26, 0 - HALF_PI, TWO_PI - HALF_PI)
+        let pxStart = cos(angleStart) * (radius) + originx
+        let pyStart = sin(angleStart) * (radius) + originy
 
-            if (i == (word.length - 1)) {
+        let letterEnd = alphabet.indexOf(word[i + 1])
+        let angleEnd = map(letterEnd, 0, 26, 0 - HALF_PI, TWO_PI - HALF_PI)
+        let pxEnd = cos(angleEnd) * (radius) + originx
+        let pyEnd = sin(angleEnd) * (radius) + originy
 
-            } else {
-                stroke(255, 255, 255, 4)
-                line(pxStart, pyStart, pxEnd, pyEnd)
-            }
+        if (i == (word.length - 1)) {
+
+        } else {
+            line(pxStart, pyStart, pxEnd, pyEnd)
         }
     }
+
 }
 
 function draw() {
